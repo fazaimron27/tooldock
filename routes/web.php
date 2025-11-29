1e29a75960742de658d16f87522391f30d03c63e
fazaimron27 <cincaumyself@gmail.com>
2025-11-29 07:45:16 +0700

    chore: update routes and ziggy configuration

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    Gate::authorize('view dashboard');
+    Gate::authorize('core.dashboard.view');
 
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
