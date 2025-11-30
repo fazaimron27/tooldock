4e0902a73b871fe14b32f4ce1a67b1b6c97d075d
fazaimron27 <cincaumyself@gmail.com>
2025-12-01 02:19:59 +0700

    feat(profile): add avatar upload functionality to user profile

--- Modules/Core/app/Http/Requests/ProfileUpdateRequest.php
+++ Modules/Core/app/Http/Requests/ProfileUpdateRequest.php
@@ -25,6 +25,7 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'avatar_id' => ['nullable', 'integer', 'exists:media_files,id'],
         ];
     }
 }
