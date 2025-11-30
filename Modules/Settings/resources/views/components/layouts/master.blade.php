0d1cca14c05efb771d57920c59f5d0016f82f233
fazaimron27 <cincaumyself@gmail.com>
2025-11-30 23:38:08 +0700

    feat(modules): update module blade templates to use settings app name
    - Replace config('app.name') with settings('app_name') in all module master layouts
    - Update module stub template for consistency
    - Ensure all modules display app name from settings

--- Modules/Settings/resources/views/components/layouts/master.blade.php
+++ Modules/Settings/resources/views/components/layouts/master.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <title>Settings Module - {{ config('app.name', 'Laravel') }}</title>
+        <title>Settings Module - {{ settings('app_name', config('app.name', 'Laravel')) }}</title>
 
         <meta name="description" content="{{ $description ?? '' }}">
         <meta name="keywords" content="{{ $keywords ?? '' }}">
