947e3ebbea568e561f85fef22373a5fc4dd2ba2b
fazaimron27 <cincaumyself@gmail.com>
2025-12-06 15:12:34 +0700

    feat(core): add custom Permission and Role models with UUID support

---
+++ Modules/Core/app/Models/Permission.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\Core\App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+class Permission extends SpatiePermission
+{
+    use HasUuids;
+
+    /**
+     * The data type of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+}
