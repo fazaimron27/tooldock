c20f21d38cb05649308339f4e6412f086fee32e1
fazaimron27 <cincaumyself@gmail.com>
2025-12-06 13:51:07 +0700

    refactor(groups): remove Blog module references from seeder

--- Modules/Groups/database/seeders/GroupsDatabaseSeeder.php
+++ Modules/Groups/database/seeders/GroupsDatabaseSeeder.php
@@ -6,15 +6,13 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Modules\Core\App\Models\User;
 use Modules\Groups\Models\Group;
-use Spatie\Permission\Models\Permission;
 
 class GroupsDatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
-     * Creates test/demo data: an "Editors" group with blog.posts.create permission,
-     * and a test user "John" assigned to the group with no roles.
+     * Creates test/demo data: an "Editors" group and a test user "John" assigned to the group with no roles.
      *
      * Note: The "Guest" group is automatically created via GroupRegistry during
      * module installation/enabling, so it's not created here.
@@ -26,15 +24,9 @@ class GroupsDatabaseSeeder extends Seeder
             ['slug' => 'editors'],
             [
                 'name' => 'Editors',
-                'description' => 'Group for content editors who can create blog posts',
+                'description' => 'Group for content editors',
             ]
         );
-
-        // Assign blog.posts.create permission to the group
-        $permission = Permission::where('name', 'blog.posts.create')->first();
-        if ($permission && ! $editorsGroup->permissions()->where('permissions.id', $permission->id)->exists()) {
-            $editorsGroup->permissions()->attach($permission->id);
-        }
 
         // Create or get user "John" with no roles
         $john = User::withoutEvents(function () {
